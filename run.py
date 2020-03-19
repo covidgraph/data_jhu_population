7d01b1251c66f46de6dd97fb2b14b7be8fb43eed
Martin <martin.preusse@gmail.com>
2020-03-19 14:37:56 +0100

    path from env

--- run.py
+++ run.py
@@ -6,30 +6,36 @@ logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('py2neo.connect.bolt').setLevel(logging.WARNING)
 logging.getLogger('py2neo.connect').setLevel(logging.WARNING)
 
-
+# import and setup
 from covid_graph import download, load_to_neo4j, helper, post
 
-ROOT_DIR = '/Users/mpreusse/Downloads/covid'
-NEO4J_HOST = 'localhost'
-NEO4J_USER = 'neo4j'
-NEO4J_PASSWORD = 'test'
+ROOT_DIR = os.getenv('ROOT_DIR')
+NEO4J_HOST = os.getenv('NEO4J_HOST')
+NEO4J_USER = os.getenv('NEO4J_USER')
+NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
 
 graph = py2neo.Graph(host=NEO4J_HOST, user=NEO4J_USER, password=NEO4J_PASSWORD)
+
 
 # setup DB
 helper.setup_db(graph)
 
 if not os.path.exists(ROOT_DIR):
     os.makedirs(ROOT_DIR)
+###
 
-# download
+# download data
 jhu_zip_file = download.download_jhu(ROOT_DIR, skip_existing=True)
 jhu_dir = download.unzip_file(jhu_zip_file, skip_existing=True)
 
 wpp_csv_file = download.download_population_data(ROOT_DIR, skip_existing=True)
+###
 
+# load to Neo4j
 load_to_neo4j.read_daily_report_JHU(jhu_dir, graph)
 load_to_neo4j.load_wpp_data(ROOT_DIR, graph)
+###
 
 # post process
 post.set_latest_update(graph)
+###
