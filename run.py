4417c8b3ce45817f67488d5aaca98afeb9d19b10
Martin <martin.preusse@gmail.com>
2020-10-02 17:37:02 +0200

    more logging on NEO4J config string parsing

--- run.py
+++ run.py
@@ -18,16 +18,19 @@ ROOT_DIR = os.getenv('ROOT_DIR', '/download')
 RUN_MODE = os.getenv('RUN_MODE', 'prod')
 
 NEO4J_CONFIG_STRING = os.getenv("NEO4J")
+log.debug(f'NEO4J config string from env (not changed): {NEO4J_CONFIG_STRING}')
+log.debug(f'Type of config string: {type(NEO4J_CONFIG_STRING)}')
 
 try:
-    log.info(NEO4J_CONFIG_STRING)
     NEO4J_CONFIG_DICT = json.loads(NEO4J_CONFIG_STRING)
 except json.decoder.JSONDecodeError:
     # try to replace single quotes with double quotes
     # JSON always expects double quotes, common mistake when writing JSON strings
     NEO4J_CONFIG_STRING = NEO4J_CONFIG_STRING.replace("'", '"')
-    log.info(NEO4J_CONFIG_STRING)
     NEO4J_CONFIG_DICT = json.loads(NEO4J_CONFIG_STRING)
+
+log.debug("NEO4J_CONFIG_DICT: ")
+log.debug(NEO4J_CONFIG_DICT)
 
 if RUN_MODE.lower() == 'test':
     import pytest
